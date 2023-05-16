04fd739449ce6bea3b58d66ec1a072c382b5265f
Jihoon <21qqq4@gmail.com>
2023-05-17 02:37:50 +0900

    플레이어 이동 및 애니메이션, 점프 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Player Parameters
-    [SerializeField] float speed;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotSpeed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float jumpHeight;
 
     CharacterController cc;
     Animator animator;
     [SerializeField] GameObject head;
 
+    // Vector Parameters for Move, Rotate
     Vector3 dir;
     Vector2 horizontal;
+
+    // State Parameters for Animation
+    enum State
+    {
+        Idle,
+        Move,
+        Jump,
+    }
+    State state;
+
+    // Target Object for Jump
+    Transform target;
 
     void Start()
     {
@@ -21,22 +37,24 @@ public class PlayerController : MonoBehaviour
 
         dir = Vector3.forward;
         horizontal = Vector2.zero;
+
+        state = State.Idle;
     }
 
     void Update()
     {
-        Move();   
+        Rotate();
+        Move();
+        Anim();
     }
 
-    void Move()
+    void Rotate()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0 && state != State.Jump)
         {
-            cc.Move(dir.normalized * speed * Time.deltaTime);
-
-            horizontal += Input.GetTouch(0).deltaPosition;
+            horizontal += Input.GetTouch(0).deltaPosition / 100;
             horizontal.y = 0;
-            Debug.Log("Asdf");
+            Debug.Log(horizontal);
             if (horizontal.magnitude > 1)
             {
                 horizontal = horizontal.normalized;
@@ -44,5 +62,87 @@ public class PlayerController : MonoBehaviour
 
             dir = (Vector3)horizontal + Vector3.forward;
         }
+        else if (Input.touchCount <= 0 && state != State.Jump)
+        {
+            dir = Vector3.forward;
+            horizontal = Vector2.zero;
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), rotSpeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        if (Input.touchCount > 0 && state != State.Jump)
+        {
+            cc.Move(dir * moveSpeed * Time.deltaTime);
+            state = State.Move;
+        }
+        else if (Input.touchCount <= 0 && state != State.Jump)
+            state = State.Idle;
+
+        if (state != State.Jump)
+            cc.Move(-transform.up * 0.1f * Time.deltaTime);
+    }
+
+    public void StartJump(Transform target)
+    {
+        this.target = target;
+        if (state != State.Jump)
+            StartCoroutine(Jump());
+    }
+
+    IEnumerator Jump()
+    {
+        state = State.Jump;
+        cc.enabled = false;
+
+        Vector3 pos;
+        Vector3 point1 = transform.position;
+        Vector3 point2 = transform.position + (target.position - point1) / 2 + Vector3.up * jumpHeight;
+        Vector3 point3 = target.position;
+
+        float currentTime = 0;
+
+        while (Vector3.Distance(transform.position, target.position) > 0.03f)
+        {
+            currentTime += Time.deltaTime;
+            yield return null;
+
+            pos = transform.position;
+            pos.y = 0;
+            dir = target.position - pos;
+            dir.Normalize();
+
+            transform.position = Bezier2D(point1, point2, point3, currentTime * jumpSpeed / 10);
+        }
+
+        state = State.Idle;
+        cc.enabled = true;
+    }
+
+    void Anim()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                animator.SetInteger("State", 0);
+                break;
+            case State.Move:
+                animator.SetInteger("State", 1);
+                break;
+            case State.Jump:
+                animator.SetInteger("State", 2);
+                break;
+        }
+    }
+
+    Vector3 Bezier2D(Vector3 p1, Vector3 p2, Vector3 p3, float ratio)
+    {
+        Vector3 p01 = Vector3.Lerp(p1, p2, ratio);
+        Vector3 p02 = Vector3.Lerp(p2, p3, ratio);
+        Vector3 p03 = Vector3.Lerp(p01, p02, ratio);
+
+        return p03;
     }
 }
