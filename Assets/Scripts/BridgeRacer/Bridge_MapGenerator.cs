44ec17d0a1ed610824c59dc07d992e481dd18b23
Jihoon <21qqq4@gmail.com>
2023-05-22 01:43:58 +0900

    벽돌 자동 생성 알고리즘 구현

--- Assets/Scripts/BridgeRacer/Bridge_MapGenerator.cs
+++ Assets/Scripts/BridgeRacer/Bridge_MapGenerator.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Bridge_MapGenerator : MonoBehaviour
 {
+    [SerializeField] GameObject brick;
+
+    float xPos = -4;
+    float zPos = 14;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        while (zPos >= 0)
+        {
+            int ran = Random.Range(0, 3);
+            GameObject go = Instantiate(brick, transform);
+            go.transform.localPosition = new Vector3(xPos, 0.1f, zPos);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (xPos < 4)
+                xPos += 2;
+            else
+            {
+                xPos = -4;
+                zPos -= 1;
+            }
+
+            switch (ran)
+            {
+                case 0:
+                    go.GetComponent<MeshRenderer>().material.color = Color.green;
+                    break;
+                case 1:
+                    go.GetComponent<MeshRenderer>().material.color = Color.red;
+                    break;
+                case 2:
+                    go.GetComponent<MeshRenderer>().material.color = Color.blue;
+                    break;
+            }
+        }
     }
 }
